87a505bb4bf53e8f3f3f9011667d9fdef4306f9b
hhf7 <1399859803@qq.com>
2022-08-30 22:44:56 +0800

    bug修复

--- src/service/post.service.ts
+++ src/service/post.service.ts
@@ -122,7 +122,7 @@ class PostService {
           p.creator LIKE '%${params.keyword}%'
         )`
         : ``
-    }
+      }
     ${params.keyword && params.categoryId ? 'AND' : ''}
     ${
       // 查询分类
@@ -132,12 +132,35 @@ class PostService {
           p.category_ids LIKE '%,${params.categoryId}%'
           )`
         : ``
-    }
+      }
     ORDER BY ${Number(params.queryType) === 1 ? 'p.create_time' : 'p.pv'} DESC
     LIMIT ${(pageNum - 1) * pageSize}, ${pageSize};
     `
 
-    const query = `select FOUND_ROWS() as count;`
+    const query = `SELECT count(*) AS count 
+    FROM post AS p
+    ${params.keyword || params.categoryId ? 'WHERE ' : ''}
+    ${
+      // 关键字查询
+      params.keyword
+        ? `(
+          p.post_name LIKE '%${params.keyword}%' || 
+          p.post_intro LIKE '%${params.keyword}%' || 
+          p.content LIKE '%${params.keyword}%' || 
+          p.creator LIKE '%${params.keyword}%'
+        )`
+        : ``
+      }
+    ${params.keyword && params.categoryId ? 'AND' : ''}
+    ${
+      // 查询分类
+      params.categoryId
+        ? `(p.category_ids LIKE '%,${params.categoryId},%' || 
+          p.category_ids LIKE '%${params.categoryId},%' || 
+          p.category_ids LIKE '%,${params.categoryId}%'
+          )`
+        : ``
+      }`
     const result = await connection.execute(statement)
     const result1 = await connection.execute(query)
     const count = result1[0][0].count
