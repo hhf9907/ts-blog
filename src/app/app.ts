3a8806c950ff09e3b9a118c6ddab12febfda5ab7
hhf7 <1399859803@qq.com>
2022-09-03 22:44:18 +0800

    跨域配置

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
 import Koa from 'koa'
 
 const bodyParser = require('koa-body')
-const cors = require('koa2-cors')
 
 import useRoutes from '../router'
 import errorHandler from './error-handle'
 
- 
+
 const app = new Koa()
 
 
@@ -22,21 +21,6 @@ app.use(
   })
 )
 
-//跨域
-app.use(
-  cors({
-    origin: function () {
-      //设置允许来自指定域名请求
-      return '*' // 允许来自所有域名请求
-      // return 'http://localhost:8080'; //只允许http://localhost:8080这个域名的请求
-    },
-    maxAge: 20, //指定本次预检请求的有效期，单位为秒。
-    credentials: false, //是否允许发送Cookie
-    allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], //设置所允许的HTTP请求方法'
-    allowHeaders: ['Content-Type', 'Authorization', 'Accept'], //设置服务器支持的所有头信息字段
-    exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'] //设置获取其他自定义字段
-  })
-)
 
 app.on('error', errorHandler)
 
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,17 @@
 const fs = require('fs')
 import Koa from 'koa'
+const cors = require('koa2-cors')
 
 const useRoutes = function (app: Koa) {
+  app.use(cors())
   fs.readdirSync(__dirname).forEach((file: string) => {
     if (file === 'index.ts') return
     const router = require(`./${file}`)
-    
+
     app.use(router.routes())
     app.use(router.allowedMethods())
   })
+
 }
 
 export default useRoutes
